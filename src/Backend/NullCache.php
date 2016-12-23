8238a969a3953acc9a5b56aa41a6491df7ef1800
Marc Rubio <marc.rubio@teamcmp.com>
2016-12-23 10:54:59 +0100

    Add null cache backend

---
+++ src/Backend/NullCache.php
@@ -0,0 +1,91 @@
+<?php
+namespace Cmp\Cache\Backend;
+
+use Cmp\Cache\Exceptions\ExpiredException;
+use Cmp\Cache\Exceptions\NotFoundException;
+use Cmp\Cache\Traits\MultiCacheTrait;
+
+/**
+ * Class NullCache
+ *
+ * A noop cache backend
+ *
+ * @package Cmp\Cache\Backend
+ */
+class NullCache extends TaggableCache
+{
+    use MultiCacheTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($key, $item, $timeToLive = null)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($key, $default = null)
+    {
+        return $default;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($key)
+    {
+        return false;
+    }
+
+    /**
+     * Determines whether an item is in the cache or not
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function has($key)
+    {
+        return false;
+    }
+
+    /**
+     * Returns an item from the cache, it throws an exception if the item is not in the cache or it has expired
+     *
+     * @param string $key
+     *
+     * @return mixed
+     *
+     * @throws NotFoundException When the item is not present in the cache
+     * @throws ExpiredException  When the item has expired
+     */
+    public function demand($key)
+    {
+        throw new NotFoundException($key);
+    }
+
+    /**
+     * Empties the cache
+     *
+     * @return bool
+     */
+    public function flush()
+    {
+        return true;
+    }
+
+    /**
+     * Gets the remaining time to live for an item
+     *
+     * @param $key
+     *
+     * @return int|null
+     */
+    public function getTimeToLive($key)
+    {
+        return null;
+    }
+}
--- tests/spec/Factory/CacheFactorySpec.php
+++ tests/spec/Factory/CacheFactorySpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Cmp\Cache\Factory;
 
+use Cmp\Cache\Backend\NullCache;
 use Cmp\Cache\Cache;
 use PhpSpec\ObjectBehavior;
 use Redis;
@@ -17,6 +18,11 @@ class CacheFactorySpec extends ObjectBehavior
     function it_build_an_array_cache()
     {
         $this->arrayCache()->shouldBeAnInstanceOf('\Cmp\Cache\Backend\ArrayCache');
+    }
+
+    function it_can_build_a_null_cache()
+    {
+        $this->nullCache()->shouldBeAnInstanceOf(NullCache::class);
     }
 
     function it_can_build_a_redis_cache(Redis $redis)
